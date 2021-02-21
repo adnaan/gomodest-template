25dccd8497e96479de9a378c9377eebfb5661fb5
Adnaan Badr <adnaan.badr@taxfix.de>
2021-02-21 11:43:45 +0100

    add turbo, update go.mod

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,6 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import * as Turbo from "@hotwired/turbo"
 import "./styles.scss";
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/adnaan/renderlayout v0.0.1
 	github.com/go-chi/chi v1.5.2
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 )
