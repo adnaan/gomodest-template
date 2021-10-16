23407f9f8a76ef81174538134ca905d46b7db370
Adnaan Badr <badr.adnaan@gmail.com>
2021-10-16 23:53:06 +0530

    simplify switching todo modes

---
+++ assets/src/controllers/todo-mode-controller.js
@@ -0,0 +1,46 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["view","edit","delete"]
+    static values = {
+        hiddenClass: { type: String, default: 'is-hidden' },
+        mode: {type: String, default: 'view'},
+    }
+
+    view(){
+        this.modeValue = "view"
+    }
+
+    edit(){
+        this.modeValue = "edit"
+    }
+
+    delete(){
+       this.modeValue = "delete"
+    }
+
+    modeValueChanged(){
+        this.showCurrentMode()
+    }
+
+    showCurrentMode(){
+        console.log(this.modeValue);
+        switch (this.modeValue) {
+            case "view":
+                this.viewTarget.classList.remove(this.hiddenClassValue);
+                this.editTarget.classList.add(this.hiddenClassValue);
+                this.deleteTarget.classList.add(this.hiddenClassValue);
+                break;
+            case "edit":
+                this.viewTarget.classList.add(this.hiddenClassValue);
+                this.editTarget.classList.remove(this.hiddenClassValue);
+                this.deleteTarget.classList.add(this.hiddenClassValue);
+                break;
+            case "delete":
+                this.viewTarget.classList.add(this.hiddenClassValue);
+                this.editTarget.classList.add(this.hiddenClassValue);
+                this.deleteTarget.classList.remove(this.hiddenClassValue);
+                break;
+        }
+    }
+}
