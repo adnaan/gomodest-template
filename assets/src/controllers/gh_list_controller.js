02bf271dde57a74d7c554a0c97941c704b604304
Adnaan Badr <badr.adnaan@gmail.com>
2021-10-17 23:54:29 +0530

    fix missing package

--- assets/src/controllers/gh_list_controller.js
+++ assets/src/controllers/gh_list_controller.js
@@ -1,5 +1,4 @@
-import {Controller} from "@hotwired/stimulus"
-import sortedIndexBy from 'lodash.sortedindexby';
+import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
     static targets = ["item"]
